e4a9bd93e72d2198d36599f89c48321e80e5d263
guyklainer <guyklainer@gmail.com>
2018-09-03 14:36:35 +0300

    - clean App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,11 @@
-import {Button, Intent, Spinner} from "@blueprintjs/core";
 import * as React from "react";
 import TodoList from "./components/todo";
 
 export default class App extends React.Component {
 	render() {
-		const mySpinner = <Spinner intent={Intent.PRIMARY}/>;
-		const btn       = <Button icon="cloud" text="my texxt"/>;
-		const todoList  = <TodoList />;
-
 		return <React.Fragment>
-			{btn}
-			{mySpinner}
 			<h1>This is first my React app</h1>
-			{todoList}
+			<TodoList />
 		</React.Fragment>
 	}
 }
--- src/app/commons/IAction.ts
+++ src/app/commons/IAction.ts
@@ -1,8 +1,8 @@
-import { Action } from "redux";
+import {Action} from "redux";
 
 interface IAction<E, T> extends Action<E> {
-    type: E;
-    payload: T;
+	type: E;
+	payload: T;
 }
 
 export default IAction;
